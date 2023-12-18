fd892a1682ea9c70b69d59a6a65aecc2b4c930d1
tghill <tghill@uwaterloo.ca>
2023-12-18 13:07:01 -0800

    Setup.py for installing local code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modules',
+    version='1.0',
+    description='My useful description',
+    author='Tim Hill',
+    author_email='tha111@sfu.ca',
+    packages=['modules/'],
+    license='MIT'
+    )
